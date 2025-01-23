94c8fe8fb22025def3cc0af64e83b18a031ecda6
fergus-g <fergus.gildea@utrack.com>
2025-01-23 16:29:00 +0000

    Added functionality for flashcards and flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Header from '../src/Header/Header.jsx'
+import "./App.css";
+import Header from "../src/Header/Header.jsx";
+import Flashcards from "./Flashcards/Flashcards.jsx";
 
 function App() {
-
   return (
-    <Header/>
-  )
+    <>
+      <Header />
+      <Flashcards />
+    </>
+  );
 }
 
-export default App
+export default App;
